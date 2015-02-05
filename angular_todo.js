40749c52b091bc8eef10a527bce87e8382f92a2a
Jon Pabico <jbpabico@gmail.com>
2015-02-05 08:06:41 -0800

    added Firebase and Bootstrap

--- angular_todo.js
+++ angular_todo.js
@@ -1,13 +1,18 @@
-function TodoCtrl($scope) {
+angular.module('todoapp', ['firebase']);
 
-    $scope.todos = [
-        {done: false, text: "first"}, 
-        {done: false, text: "second"},
-        {done: false, text: "3rd"},
-        {done: false, text: "4th"},
-        {done: false, text: "5th"}
-    ];
+// scope is an object shared with html; angularFire library is a dependency
+function TodoCtrl($scope, angularFire) {
 
+    // Firebase object representing data
+    var fireData = new Firebase('https://angular-todo-pabico.firebaseio.com');
+
+    // binds fireData to $scope.todos
+    angularFire(fireData, $scope, 'todos');
+
+    // initialize empty todo list
+    $scope.todos = [];
+
+    // adds a new list item
     $scope.addTodo = function() {
         var newTodo = {
             done: false, text: $scope.todoText
@@ -16,25 +21,24 @@ function TodoCtrl($scope) {
         $scope.todoText = "";
     }
 
+    // removes a list item
     $scope.removeTodo = function(start) {
         $scope.todos.splice(start, 1);
     }
 
+    // allows items to be rearranged
     $scope.move = function(index, direction) {
-
         if(direction === "up") {
             if (index ===0) {
                 return;
             }
             index = index - 1;
         }
-
         if (direction === "down") {
             if (index === $scope.todos.length -1) {
                 return;
             }
         }
-
         var todo = $scope.todos[index]
         $scope.todos.splice(index + 2, 0, todo);
         $scope.todos.splice(index, 1);
