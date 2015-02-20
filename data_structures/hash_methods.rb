fcb648380ec48866a57f0f9ed9e65fd5903747bf
Jon Pabico <jbpabico@gmail.com>
2015-02-20 10:04:49 -0800

    initial commit

--- data_structures/hash_methods.rb
+++ data_structures/hash_methods.rb
@@ -1,1 +1,14 @@
-# not yet started
+# Some useful methods for hashes in Ruby
+# see documentation for complete set of methods
+
+a = {first_name: "joe", last_name: "smith"}
+a[:middle_name] = "michael"
+
+b = Hash.new            # no default value
+b[:age] = 4
+b
+
+c = Hash.new(3)        # default value of 3 for new keys
+c["s"]+=22
+c
+
