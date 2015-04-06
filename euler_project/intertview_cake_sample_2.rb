3b00e581248d508230f18a270ac34b95b893bcbe
Jon Pabico <jbpabico@gmail.com>
2015-04-05 20:53:37 -0700

    interview cake sample problem 2

---
+++ euler_project/intertview_cake_sample_2.rb
@@ -0,0 +1,4 @@
+# Problem: given an array of integers, write a functions that finds the product of every integer except the integer at that index. 
+# Example: given a = [1, 7, 3, 4],  the function would return [84, 12, 28, 21] by calculating [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+# Additional notes: do not use division in the soultion
+
