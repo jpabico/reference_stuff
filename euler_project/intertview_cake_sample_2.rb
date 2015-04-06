daacd01971a150aea99951eda3fceb7520564e64
Jon Pabico <jbpabico@gmail.com>
2015-04-05 21:03:29 -0700

    solution for interview cake sample prob #2

--- euler_project/intertview_cake_sample_2.rb
+++ euler_project/intertview_cake_sample_2.rb
@@ -2,3 +2,20 @@
 # Example: given a = [1, 7, 3, 4],  the function would return [84, 12, 28, 21] by calculating [7*3*4, 1*3*4, 1*7*4, 1*7*3]
 # Additional notes: do not use division in the soultion
 
+def product_array(input_array)
+    return_array = []
+    input_array.each_with_index do |x, i|
+        # puts "x is #{x} and i is #{i}"
+        product = 1
+        input_array.each_with_index do |y,j|
+            if i!=j
+                product *= y
+            end
+        end
+        return_array << product
+    end
+    return return_array
+end
+
+a = [1, 7, 3, 4]
+product_array(a)
