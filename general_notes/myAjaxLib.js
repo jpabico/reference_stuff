ed6d3cf7b3af6c0b8352c82d6fbade284cc3e713
Jon Pabico <jbpabico@gmail.com>
2015-04-11 22:45:57 -0700

    example from JS textbook pt 2

--- general_notes/myAjaxLib.js
+++ general_notes/myAjaxLib.js
@@ -12,3 +12,22 @@ function getXMLHttpRequest() {
     }
 }
 
+function doAjax(url, query, callback, reqtype, getxml) {
+    var myreq = getXMLHttpRequest();
+    myreq.onreadystatechange = function() {
+        if(myreq.readyState==4) {
+            if(myreq.status==200) {
+                var item = myreq.responseText;
+                if(getxml==1) {
+                    item = myreq.responseXML;
+                    eval(callback + '(item)');
+                }
+            }
+        }
+    }
+    if(reqtype.toUpperCase()=='POST') {
+        requestPOST(url, query, myreq);
+    } else {
+        requestGET(url,query, myreq);
+    }
+}
