fd362a47b4a3a1f0e0a8a1a0b170647a214196ec
Jon Pabico <jbpabico@gmail.com>
2015-04-16 21:51:16 -0700

    state problem 2

---
+++ interview_code/symni_2.js
@@ -0,0 +1,11 @@
+// Assuming you have the HTML below and jQuery available, please write the JavaScript that
+//  *    makes it so that when the button is clicked, the background color of the div changes to
+//  *    the next color in this array: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'].
+//  *    Each successive click should then move to the next color, and after one full cycle through the
+//  *    array, it should begin again at the first color in the array. However, these color changes
+//  *    should be throttled so that it can only be changed 0.5 seconds or later after the last change.
+
+//  * Available HTML:
+//  *   <div id="main">Main Div</div>
+//  *   <button id="button">Rainbow Time!</button>
+//  */
