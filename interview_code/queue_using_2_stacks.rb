09f4d10458ed9bddfbe4e51a2fe8374922f9bddc
Jon Pabico <jbpabico@gmail.com>
2015-04-20 23:16:00 -0700

    problem statement

---
+++ interview_code/queue_using_2_stacks.rb
@@ -0,0 +1,19 @@
+# Implement a Stack class using two queues.
+
+# You may assume you have a queue class with the five methods listed below.
+
+# Queue: First in, first out
+
+# first
+# last
+# enqueue(item) #add
+# dequeue #remove
+# is_empty?
+
+# Stack: Last in, first out
+
+# peek
+# pop #remove
+# push(item) #add
+# is_empty?
+
