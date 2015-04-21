5bfde9b13deac6dc7164dec7f2293da9aec5c9d4
Jon Pabico <jbpabico@gmail.com>
2015-04-20 23:56:04 -0700

    new name

---
+++ interview_code/queue_using_2_stacks.rb
@@ -0,0 +1,100 @@
+# Implement a Stack class using two queues.
+
+# You may assume you have a queue class with the five methods listed below.
+
+# Queue: First in, first out
+
+# first
+# last
+# enqueue(item) #add
+# dequeue #remove
+# is_empty?
+
+# Stack: Last in, first out
+
+# peek
+# pop #remove
+# push(item) #add
+# is_empty?
+
+
+class Kyoo
+
+    def initialize
+        @queue = []
+    end
+
+    def first
+        @last_index = @queue.length  - 1
+        return @queue[@last_index]
+    end
+
+    def last
+        @first_index = 0
+        return @queue[@first_index]
+    end
+
+    def enqueue(item)
+        @queue.unshift(item)
+    end
+
+    def dequeue
+        @queue.pop
+    end
+
+    def is_empty?
+        if @queue.length ==0
+            return true
+        else
+            return false
+        end
+    end
+        
+    end
+
+    
+end
+
+class Staakk
+
+    def initialize
+        @queue1 = Kyoo.new
+        @queue2 = Kyoo.new
+    end
+
+    def add(item)
+        if @queue1.length==0
+            @queue2.unshift(item)
+        else
+            @queue1.unshift(item)
+        end
+    end
+
+    def remove
+
+    end
+
+    def is_empty?
+        if @queue1.length == 0 && @queue2.length == 0
+            return true
+        else
+            return false
+        end
+    end
+
+end
+
+a = Kyoo.new
+a.enqueue('a')
+a.enqueue('b')
+a.enqueue('c')
+a.first
+a.last
+a.dequeue
+a.display
+a.dequeue
+a.is_empty?
+a.dequeue
+a.is_empty?
+b = Staakk.new
+b.display
