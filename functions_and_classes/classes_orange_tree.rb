bbd336ffd02403cc15b945d1b89edef735df216a
Jon Pabico <jbpabico@gmail.com>
2015-04-22 22:40:54 -0700

    methods listed

--- functions_and_classes/classes_orange_tree.rb
+++ functions_and_classes/classes_orange_tree.rb
@@ -1,4 +1,26 @@
 class Tree
+
+    # certain age and height
+    def initialize
+    end
+
+    # age and height increase
+    # dies at certain age
+    def grow
+    end
+
+    # show number of fruit
+    # must reach fruit0bearing age first
+    def num_fruit
+    end
+
+    # fruit picked from tree
+    def pick
+    end
+
+
+
+
 end
 
 class Orange
