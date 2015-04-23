db576994ffa8ae8c4dc9b1492c8bdebede1bdab5
Jon Pabico <jbpabico@gmail.com>
2015-04-23 00:34:40 -0700

    defined basic methods

--- functions_and_classes/classes_orange_tree.rb
+++ functions_and_classes/classes_orange_tree.rb
@@ -2,26 +2,33 @@ class Tree
 
     # certain age and height
     def initialize
+        @age = 0
+        @height = 0
+        @num_fruit = 0
     end
 
     # age and height increase
     # dies at certain age
     def grow
+        @height += 2
+        @age = 1
     end
 
     # show number of fruit
     # must reach fruit0bearing age first
-    def num_fruit
+    def bear_fruit
+        if @age < 3
+            @num_fruit = 0
+        elsif @age >=3 && @age < 10
+            @num_fruit += 4
+        else
+            @num_fruit += 0
+        end
     end
 
     # fruit picked from tree
     def pick
+        @num_fruit = 0
     end
-
-
-
-
 end
 
-class Orange
-end
