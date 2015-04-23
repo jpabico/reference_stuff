1c91705f5435ec5f67b820ab3d5db66616236a57
Jon Pabico <jbpabico@gmail.com>
2015-04-23 00:56:49 -0700

    increase values of variables in one location

--- functions_and_classes/classes_orange_tree.rb
+++ functions_and_classes/classes_orange_tree.rb
@@ -1,34 +1,68 @@
 class Tree
-
+    attr_accessor :age, :height, :num_fruit, :alive
     # certain age and height
     def initialize
         @age = 0
         @height = 0
         @num_fruit = 0
+        @alive = true
     end
 
     # age and height increase
     # dies at certain age
     def grow
-        @height += 2
-        @age = 1
-    end
-
-    # show number of fruit
-    # must reach fruit0bearing age first
-    def bear_fruit
-        if @age < 3
-            @num_fruit = 0
-        elsif @age >=3 && @age < 10
-            @num_fruit += 4
+        if @alive
+            @age += 1
+            if @age > 10
+                @alive = false
+            elsif @age < 3
+                @num_fruit = 0
+                @height += 2
+            elsif @age >=3
+                @num_fruit += 4
+            end
         else
             @num_fruit += 0
+            @alive = false
         end
     end
 
     # fruit picked from tree
-    def pick
+    def pick_fruit
         @num_fruit = 0
+    end
+
+    def to_s
+        puts "Age of tree is: " + @age.to_s
+        puts "Height of tree is " + @height.to_s
+        puts "Number of pieces of fruit is " + @num_fruit.to_s
+        puts "Tree is alive... " + @alive.to_s
     end
 end
 
+a = Tree.new
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.pick_fruit
+a.to_s
+a.grow
+a.to_s
+a.grow
+a.pick_fruit
+a.to_s
+a.grow
+a.to_s
+a.to_s
+a.grow
