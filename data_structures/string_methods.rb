0eb9c9c0c6e837064843ee63d56e843abce2a8e1
Jon Pabico <jbpabico@gmail.com>
2015-04-26 23:37:49 -0700

    substitution

---
+++ data_structures/string_methods.rb
@@ -0,0 +1,7 @@
+a = "This is a Sample string with CAPS and all?"
+b = 'This one\'s got an escaped character'
+x, y, z = 12, 24, 48
+
+puts "the value of x is #{x}"
+puts "the sum of x and y is #{ x + y}"
+puts "the average of x, y, and z is #{ (x+y+z)/3}"
