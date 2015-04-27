8975a6eb73ee460a69a85e80492072141212b664
Jon Pabico <jbpabico@gmail.com>
2015-04-26 23:52:03 -0700

    common methods

--- data_structures/string_methods.rb
+++ data_structures/string_methods.rb
@@ -1,5 +1,7 @@
 a = "This is a Sample string with CAPS and all?"
-b = 'This one\'s got an escaped character'
+b = 'this one\'s got an escaped character'
+c = "hey"
+d = "dude"
 x, y, z = 12, 24, 48
 
 puts "the value of x is #{x}"
@@ -7,3 +9,15 @@ puts "the sum of x and y is #{ x + y}"
 puts "the average of x, y, and z is #{ (x+y+z)/3}"
 
 %{Ruby is fun.}    # "Ruby is fun"
+
+# common methods
+a.downcase  # "this is a sample string with caps and all?"
+c * 4   # "heyheyheyhey"
+c + d   # "heydude"
+c << d   # "heydude"
+c <=> d   # 1
+d <=> c   # -1
+b.capitalize
+a.chomp   # removes \n from the end of a string
+d.chop   # "dud"
+b.count("c")   # 3... because there are 3 c's
