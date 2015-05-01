f5046e791f1454e40ed5bf8bfdcc358217d51386
Jon Pabico <jbpabico@gmail.com>
2015-04-30 22:32:24 -0700

    experiment with setTimeout

--- general_notes/node_notes/hello_world_node.js
+++ general_notes/node_notes/hello_world_node.js
@@ -4,3 +4,8 @@ http.createServer(function(req, res) {
     res.end('Hello World!\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at localhost:1337');
+
+// setTimeout(function() {
+//     console.log("world");
+// }, 2000);
+// console.log("hello");
