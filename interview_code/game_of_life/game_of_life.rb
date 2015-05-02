ab7f6d4db9fb9a96e27be10bda4f283c49ad973f
Jon Pabico <jbpabico@gmail.com>
2015-05-02 11:54:08 -0700

    set up world and cell classes

---
+++ interview_code/game_of_life/game_of_life.rb
@@ -0,0 +1,46 @@
+# Conway's Game of Life
+
+# rules of game
+
+# each cell has 8 neighbors
+# any live cell with 0 or 1 live neighbors will die (underpopulation)
+# any live cell with 2 or 3 live neighbors will live to next generation
+# any live cell with more than 3 live neighbors will die (overpopulation)
+# any dead cell with exactly 3 live neighbors will become alive (reproduction)
+
+class World
+
+    attr_accessor :rows, :cols, :cell_grid
+    def initialize(rows=3, cols=3)
+        @rows = rows
+        @cols = cols
+
+        @cell_grid = Array.new(rows) do |row|
+            Array.new(cols) do |col|
+                Cell.new
+            end
+        end
+
+    end
+
+end
+        
+    # def rows
+    # end
+
+    # def cols
+    # end
+
+
+
+class Cell
+    attr_accessor :alive, :x, :y
+    def initialize(x=0, y=0)
+        @alive = false
+        @x = x
+        @y = y
+
+    end
+end
+
+
---
+++ interview_code/spec_game_of_life.rb
@@ -0,0 +1,5 @@
+require 'rspec'
+
+describe 'Game of life' do
+    puts "hi"
+end
