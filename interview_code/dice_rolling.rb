15e927b8fac3d7a7634cc19a14a41ffbb347350b
Jon Pabico <jbpabico@gmail.com>
2015-05-06 22:10:34 -0700

    psuedocode

--- interview_code/dice_rolling.rb
+++ interview_code/dice_rolling.rb
@@ -1,3 +1,11 @@
 # simulate rolling two dice and finding the sum
 
-# pseudocode
+# pseudocode - plan of attack
+# define class for one die 
+# give die some properties (ex. 6 sides represented by integers 1-6)
+# give die a roll method to achieve 1 of 6 outcomes
+# instantiate two dice objects
+# call roll method on each die 
+# write a sum method to find the total
+
+
