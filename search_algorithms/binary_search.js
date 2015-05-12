9ad5092e45594883bff5591f5476754b7afcde94
Jon Pabico <jbpabico@gmail.com>
2015-05-11 20:46:14 -0700

    solution for binary search in js

--- search_algorithms/binary_search.js
+++ search_algorithms/binary_search.js
@@ -8,3 +8,29 @@
 // 4 - If the guess was too low, that is, array[guess] < target, then set min = guess + 1.
 // 5 - Otherwise, the guess was too high. Set max = guess - 1.
 // 6 - Go back to step 2.
+
+var doSearch = function(array, targetValue) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    var counter = 0;
+    while(max >= min) {
+        guess = Math.floor((min + max) / 2);
+        // console.log(guess);
+        counter++;
+        if(array[guess]===targetValue) {
+            console.log("number of guesses: " + counter);
+            return guess;
+        } else if(array[guess] < targetValue) {
+            min = guess + 1;
+        } else {
+            max = guess - 1;
+        }
+    }
+    return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+console.log("Found prime at index " + result);
