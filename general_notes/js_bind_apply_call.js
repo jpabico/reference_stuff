801c0fd17f8ff1116b951bfa845f825dec27badf
Jon Pabico <jbpabico@gmail.com>
2015-06-09 13:54:18 -0700

    js bind example 3

--- general_notes/js_bind_apply_call.js
+++ general_notes/js_bind_apply_call.js
@@ -39,3 +39,23 @@ var range = { minimum: 10, maximum: 20 };
 
 // 'this' will now refer to 'range'
 console.log(originalObject.checkNumericRange.bind(range)(10));   // true
+
+// example 3
+
+// The following code shows how to use the arg1[,arg2[,argN]]] arguments. The bound function uses the parameters specified in the bind method as the first and second parameters. Any parameters specified when the bound function is called are used as the third, fourth (and so on) parameters.
+
+// Define the original function with four parameters.
+var displayArgs = function (val1, val2, val3, val4) {
+    document.write(val1 + " " + val2 + " " + val3 + " " + val4);
+}
+
+var emptyObject = {};
+
+// Create a new function that uses the 12 and "a" parameters
+// as the first and second parameters.
+var displayArgs2 = displayArgs.bind(emptyObject, 12, "a");
+
+// Call the new function. The "b" and "c" parameters are used
+// as the third and fourth parameters.
+displayArgs2("b", "c");
+// Output: 12 a b c
