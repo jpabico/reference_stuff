e0fcf2af7da679eb86a6053f947969ff2e7cfd86
Jon Pabico <jbpabico@gmail.com>
2015-06-09 13:50:26 -0700

    js bind example 2

--- general_notes/js_bind_apply_call.js
+++ general_notes/js_bind_apply_call.js
@@ -1,3 +1,5 @@
+// example 1
+
 // keyword 'this' is not defined
 var checkNumericRange = function(value) {
   if (typeof value !== 'number') {
@@ -13,3 +15,27 @@ var range = {minimum: 5, maximum: 15};
 // set 'this' equal to 'range'
 // checkNumericRange requires an argument (12)
 console.log(checkNumericRange.bind(range)(12));   // true
+
+
+// example 2
+
+//minimum and maximum define originally
+var originalObject = {
+    minimum: 50,
+    maximum: 100,
+    checkNumericRange: function (value) {
+        if (typeof value !== 'number')
+            return false;
+        else
+            return value >= this.minimum && value <= this.maximum;
+    }
+}
+
+// Check whether 10 is in the numeric range.
+console.log(originalObject.checkNumericRange(10));   // false
+
+// redefine 'this' using object 'range'
+var range = { minimum: 10, maximum: 20 };
+
+// 'this' will now refer to 'range'
+console.log(originalObject.checkNumericRange.bind(range)(10));   // true
